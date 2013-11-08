9586cca6423ea771a477074056ebe3ca0e0f3b09
JuhaMikkola <juha@Juhas-MacBook-Pro.local>
2013-11-08 12:52:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest::Application.config.secret_key_base = 'de3cfeb96af9e6fc22205aca3cf8a0cbba0dc53ba802126d37dbb8777e5d77e071cdbb867e696f3b3a13e020cdb09a92be561aea7f2b7546747909c7e41125e1'
